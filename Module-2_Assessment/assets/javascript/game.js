e00cf95824ffcf4c42e14bde165365017547d5e5
Dustin Kowal <dkowal@css.edu>
2020-05-24 19:14:43 -0500

    completed game functionality

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -9,6 +9,7 @@ var winCount = 0;
 var numGuesses;
 var currentWord;
 var guessKey;
+var correctLetter = false;
 
 const wordList = ["test", "testy", "tester", "testing", "tested"];
 
@@ -17,14 +18,20 @@ function gameSetup() {
     //resets number of guesses
     numGuesses = 12;
     guessRemain.innerHTML = numGuesses;
+
     //randomly chooses a word from wordList
     currentWord = wordList[Math.floor(Math.random() * wordList.length)];
+
+    //ensures arrays are empty when starting a new game
+    answerArray.length = 0;
+    wrongCharArray.length = 0;
 
     for (let i = 0; i < currentWord.length; i++) {
         answerArray[i] = "__";
     }
 
-    document.getElementById("currentWord").innerHTML = answerArray.join(" ");
+    lettersGuessed.innerHTML = wrongCharArray.join(" ");
+    currentWordPara.innerHTML = answerArray.join(" ");
     console.log(currentWord);
 }
 
@@ -34,25 +41,45 @@ function checkLetter() {
     for (let i = 0; i < currentWord.length; i++) {
       if (guessKey === currentWord[i]) {
         answerArray[i] = guessKey;
-        document.getElementById("currentWord").innerHTML = answerArray.join(" ");
+        currentWordPara.innerHTML = answerArray.join(" ");
+        correctLetter = true;
       }
     }
-    
+
+    //runs when answer array still has blanks
     if(answerArray.includes("__")){
-        //decreases number of guesses if it was an incorrect letter
-        if(!answerArray.includes(guessKey)){
+        
+        //checks if wrong letter is already in each array
+        if(!answerArray.includes(guessKey) && !wrongCharArray.includes(guessKey)){
+
+            wrongCharArray.push(guessKey);
+            lettersGuessed.innerHTML = wrongCharArray.join(" ");
+
+            //decreases number of guesses if it was an incorrect letter
             numGuesses--;
             guessRemain.innerHTML = numGuesses;
+            //starts new game if number of guesses hits 0
+            if(numGuesses < 1){
+                newGame();
+            }
         }
     }
     else{
         //increases wins once loop is finished if the answer array is filled with letters
-        //FIXME when program ends count increases with every key press
         winCount++;
         winCountPara.innerText = winCount;
+
+        newGame();
     }
   }
+    //testCount++;
+    //lettersGuessed.innerHTML = testCount;
 }
 
-gameSetup();
-checkLetter();
+newGame();
+
+
+function newGame(){
+    gameSetup();
+    checkLetter();
+}
